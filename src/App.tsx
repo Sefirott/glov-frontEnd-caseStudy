73dc7a1ea7abf333fcf94e9c5010521736ff8d91
Sefirott <tolgabakir17@outlook.com>
2024-05-02 01:23:14 +0300

    feat: add input field and title

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import "./App.css";
 
 function App() {
   return (
-    <main>
-      <h1>Customer Service GPT</h1>
+    <div className="main">
+      <div>
+        <h1>Customer Service GPT</h1>
+      </div>
 
-      <form className="vertical">
-        <input type="text" placeholder="Message" />
-        <input type="submit" value="Send" />
-      </form>
-    </main>
+      <div className="userResponse">
+        <form className="vertical">
+          <input type="text" placeholder="Message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
   );
 }
 
