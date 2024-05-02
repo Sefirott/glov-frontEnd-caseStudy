57354aa00b12e175301c9c48d70f57eb6a4e4f76
Sefirott <tolgabakir17@outlook.com>
2024-05-02 22:12:06 +0300

    feat: add overall css improvements to layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
 
 import "./App.css";
+interface Response {
+  message: string;
+  role: "gpt" | "customer";
+}
 
 const App = () => {
+  const [responses, setResponses] = useState<Response[]>([]);
+
   return (
     <div className="main">
-      <h1>Customer Service GPT</h1>
+      {responses.length === 0 && (
+        <div>
+          <h1>Customer Service GPT</h1>
+          <p style={{ textAlign: "center" }}>
+            Hi 👋, how can i help you today?
+          </p>
+        </div>
+      )}
+
+      {responses.map((response, index) => (
+        <div className="conversation">
+          <p key={index} className={`response-${response.role}`}>
+            {response.message}
+          </p>
+        </div>
+      ))}
 
       <div className="userResponse">
         <form className="vertical">
